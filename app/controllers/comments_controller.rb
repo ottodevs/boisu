9b794270860138a4372be9ea1b8477e74d780c58
Otto G <ottodevs@gmail.com>
2016-11-28 17:18:12 +0100

    Refactoring comments with some partials, added ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to voice_message_path(@voice_message)
   end
 
+  def destroy
+    @voice_message = VoiceMessage.find(params[:voice_message_id])
+    @comment = @voice_message.comments.find(params[:id])
+    @comment.destroy
+    redirect_to voice_message_path(@voice_message)
+  end
+
   private
   def comments_params
     params.require(:comment).permit(:author, :body)
--- app/models/voice_message.rb
+++ app/models/voice_message.rb
@@ -1,4 +1,4 @@
 class VoiceMessage < ApplicationRecord
-  has_many :comments
-  
+  has_many :comments, dependent: :destroy
+
 end
